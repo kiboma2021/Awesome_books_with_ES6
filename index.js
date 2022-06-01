00bc12f6043b909f97a2b55ccc99a4bf28440c91
Ben Kiboma <omayioben@gmail.com>
2022-06-01 11:08:10 +0300

    creating the modules

--- index.js
+++ index.js
@@ -6,33 +6,7 @@ import BookManager from "./modules/booksManager.js"
 import resetInput from "./modules/resetInput.js"
 import displayBook from "./modules/display.js"
 import displaySection from "./modules/displaySelection.js"
-
-function storageAvailable(type) {
-  let storage;
-  try {
-    const x = '__storage_test__';
-    storage = window[type];
-    storage.setItem(x, x);
-    storage.removeItem(x);
-    return true;
-  } catch (e) {
-    return (
-      e instanceof DOMException
-                // everything except Firefox
-                && (e.code === 22
-                // Firefox
-                || e.code === 1014
-                // test name field too, because code might not be present
-                // everything except Firefox
-                || e.name === 'QuotaExceededError'
-                // Firefox
-                || e.name === 'NS_ERROR_DOM_QUOTA_REACHED')
-                // acknowledge QuotaExceededError only if there's something already stored
-                && storage
-                && storage.length !== 0
-    );
-  }
-}
+import { storageAvailable } from "./modules/storage.js"
 
 const bookName = document.getElementById('name');
 const bookAuthor = document.getElementById('author');
@@ -40,7 +14,6 @@ const addBtn = document.getElementById('add');
 let books;
 
 const library = new BookManager();
-
 
 if (storageAvailable('localStorage')) {
   window.addEventListener('load', () => {
